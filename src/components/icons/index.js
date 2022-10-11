e0ab76f47d3e9b7415472bf8a21fa994e6d9f23c
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-11 10:43:35 +0300

    Close icon added with svgr

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,3 +1,4 @@
+export { default as Close } from "./Close";
 export { default as Favorite } from "./Favorite";
 export { default as History } from "./History";
 export { default as Logo } from "./Logo";
