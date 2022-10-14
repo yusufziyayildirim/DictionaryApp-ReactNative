077baa19c061e871f0ddabb6ab9a6bb1917209b1
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-14 10:58:04 +0300

    Left and More icon added with svgr

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,5 +1,7 @@
 export { default as Close } from "./Close";
 export { default as Favorite } from "./Favorite";
 export { default as History } from "./History";
+export { default as Left } from "./Left";
 export { default as Logo } from "./Logo";
+export { default as More } from "./More";
 export { default as Search } from "./Search";
