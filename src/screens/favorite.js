cd6b38b42afd96a84c689978fe9816810d79b7c4
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-19 10:47:08 +0300

    Favorite and History view designed

--- src/screens/favorite.js
+++ src/screens/favorite.js
@@ -1,13 +1,45 @@
 import FocusAwareStatusBar from "../components/FocusAwareStatusBar";
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text } from "react-native";
+import { COLORS } from "../utils/colors";
+import TitleCardList from "../components/TitleCardList";
+import SvgVector from "../components/icons/Vector";
+import { Favorite } from '../components/icons'
 
-function FavoriteView() {
+
+function FavoriteView({ navigation }) {
+
+  const FavoriteDATA = [
+    // {
+    //   id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    //   title: 'yokoğluyok',
+    //   summary: 'acaklana 1'
+    // },
+    // {
+    //   id: '3ac68afc-c685-48d3-a4f8-fbd91aa97f63',
+    //   title: 'milliyet',
+    //   summary: 'acaklang 2'
+    // },
+    // {
+    //   id: "2",
+    //   title: 'Third Item 3',
+    //   summary: 'acaklang 3'
+    // }
+  ]
   return (
-    <SafeAreaView>
+    <View style={{ backgroundColor: COLORS.softGray, flex: 1 }}>
       <FocusAwareStatusBar barStyle="dark-content" />
-      <Text>Favorite!</Text>
-    </SafeAreaView>
+      {FavoriteDATA.length > 0 ? (
+        <View style={{ flex: 1, backgroundColor: COLORS.softGray, paddingHorizontal: 18 }}>
+          <TitleCardList data={FavoriteDATA} navigation={navigation} icon={<SvgVector color={COLORS.red} />} />
+        </View>
+      ) : (
+        <View style={{ flex: 1, alignItems:"center", justifyContent:"center" }}>
+          <Favorite color={COLORS.textLight} />
+          <Text style={{fontWeight: "600", color:COLORS.textMedium, marginTop: 14}}>Henüz Favori yok </Text>
+        </View>
+      )}
+
+    </View>
   );
 }
 
--- src/screens/history.js
+++ src/screens/history.js
@@ -1,14 +1,46 @@
-import SafeAreaView from "react-native-safe-area-view";
-import { Text, View } from "react-native";
+import { View, Text } from "react-native";
 import FocusAwareStatusBar from "../components/FocusAwareStatusBar";
+import { COLORS } from "../utils/colors";
+import TitleCardList from "../components/TitleCardList";
+import SvgVector from "../components/icons/Vector";
+import { History } from '../components/icons'
+
+const HistoryDATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'Adem',
+    summary: 'acaklana 1'
+  },
+  {
+    id: '3ac68afc-c685-48d3-a4f8-fbd91aa97f63',
+    title: 'Dede',
+    summary: 'acaklang 2'
+  },
+  {
+    id: "2",
+    title: 'Göze gelmek',
+    summary: 'acaklang 3'
+  }
+]
 
 function HistoryView({ navigation }) {
   return (
-    <SafeAreaView style={{ backgroundColor: "white", flex: 1 }}>
+    <View style={{ backgroundColor: COLORS.softGray, flex: 1 }}>
       <FocusAwareStatusBar barStyle="dark-content" />
-      <Text>History!</Text>
-    </SafeAreaView>
+      {HistoryDATA.length > 0 ? (
+        <View style={{ flex: 1, backgroundColor: COLORS.softGray, paddingHorizontal: 18 }}>
+          <TitleCardList data={HistoryDATA} navigation={navigation} icon={<SvgVector color={COLORS.red} />} />
+        </View>
+      ) : (
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+          <History color={COLORS.textLight} />
+          <Text style={{ fontWeight: "600", color: COLORS.textMedium, marginTop: 14 }}>Henüz Favori yok </Text>
+        </View>
+      )}
+
+    </View>
   );
 }
+
 
 export default HistoryView;
