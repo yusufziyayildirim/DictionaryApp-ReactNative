b773fa40afcb20ac499edaeef79c919183f9c433
Yusuf Ziya YILDIRIM <101952885+yusufziyayildirim@users.noreply.github.com>
2022-10-21 09:04:22 +0300

    Data voiceover

--- src/screens/detail.js
+++ src/screens/detail.js
@@ -1,22 +1,27 @@
-import SafeAreaView from "react-native-safe-area-view";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { useEffect, useState } from "react";
+import { View, Text, ScrollView } from "react-native";
 import FocusAwareStatusBar from "../components/FocusAwareStatusBar";
 import { COLORS } from "../utils/colors";
 import { ActionButton, ActionButtonTitle } from "../components/ActionButton";
 import { Sound, Favorite, Hand, FavoriteSolid } from '../components/icons'
 import DetailSummaryItem from "../components/DetailSummaryItem";
 import LoaderText from "../components/LoaderText";
-import { useEffect, useState } from "react";
+import { Audio } from 'expo-av';
+
 
 function DetailView({ route, favoritesData, setFavoritesData }) {
   const keyword = route.params?.title;
   const [data, setData] = useState(null);
+  const [sesKod, setSesKod] = useState("");
   const [isFav, setIsFav] = useState(false);
 
   const getDetailData = async () => {
     const response = await fetch(`https://sozluk.gov.tr/gts?ara=${keyword}`)
-    const data = await response.json();
+    let data = await response.json();
     setData(data[0]);
+    data = await fetch(`https://sozluk.gov.tr/yazim?ara=${keyword}`)
+    const sesKodData = await data.json();
+    setSesKod(sesKodData[0]?.seskod);
   }
 
   const addFavData = (keyword) => {
@@ -26,11 +31,18 @@ function DetailView({ route, favoritesData, setFavoritesData }) {
       ...favoritesData
     ]);
   }
+
   const delFavData = () => {
     setIsFav(false)
     setFavoritesData(
       favoritesData.filter(favData => favData.madde.toLowerCase() !== keyword)
     );
+  }
+
+  const playAudio = async () => {
+    const { sound } = await Audio.Sound.createAsync({ uri: `https://sozluk.gov.tr/ses/${sesKod}.wav` })
+
+    await sound.playAsync();
   }
 
   useEffect(() => {
@@ -43,9 +55,9 @@ function DetailView({ route, favoritesData, setFavoritesData }) {
   }, [])
 
   return (
+
     <View style={{ backgroundColor: COLORS.softGray, flex: 1 }}>
       <FocusAwareStatusBar barStyle="dark-content" />
-
       <ScrollView style={{ padding: 16 }}>
         <View>
           <Text style={{ fontSize: 32, fontWeight: "bold" }}>{keyword}</Text>
@@ -59,9 +71,11 @@ function DetailView({ route, favoritesData, setFavoritesData }) {
 
         <View style={{ flexDirection: "row", justifyContent: "space-between", marginTop: 20 }}>
           <View style={{ flexDirection: "row", justifyContent: "space-between", width: 114 }}>
-            <ActionButton disabled={!data}>
-              <Sound width={24} height={24} color={COLORS.textDark} />
-            </ActionButton>
+            {sesKod && (
+              <ActionButton disabled={!data} onPress={() => playAudio()}>
+                <Sound width={24} height={24} color={COLORS.textDark} />
+              </ActionButton>
+            )}
             {isFav ? (
               <ActionButton onPress={() => delFavData(keyword)}>
                 <FavoriteSolid width={24} height={24} color={COLORS.textDark} />
